533fa39b68c0712db10f1d6b4c6df05f8eb9fb4e
thiago-Coderr <tiagoafonso110@gmail.com>
2025-07-23 22:20:33 +0100

    fixed rust format

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,7 @@
-use bitcoincore_rpc::{bitcoin::Address, bitcoin::Amount, bitcoin::Txid, Auth, Client, RpcApi};
+use bitcoincore_rpc::{
+    bitcoin::{Address, Amount, Txid, Network},
+    Auth, Client, RpcApi,
+};
 use serde_json::json;
 use std::fs::File;
 use std::io::Write;
@@ -6,59 +9,86 @@ use std::io::Write;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let miner_rpc = Client::new(
         "http://127.0.0.1:18443",
-        Auth::UserPass("user".to_string(), "pass".to_string()),
-    )?;
-    let trader_rpc = Client::new(
-        "http://127.0.0.1:18444",
-        Auth::UserPass("user".to_string(), "pass".to_string()),
+        Auth::UserPass("user".to_string(), "password".to_string()),
     )?;
 
-    let trader_address =
-        trader_rpc.get_new_address(None, None)?.require_network(bitcoincore_rpc::bitcoin::Network::Regtest)?;
-    let miner_address =
-        miner_rpc.get_new_address(None, None)?.require_network(bitcoincore_rpc::bitcoin::Network::Regtest)?;
+    let trader_rpc = Client::new(
+        "http://127.0.0.1:18444",
+        Auth::UserPass("user".to_string(), "password".to_string()),
+    )?;
+
+    let trader_address = trader_rpc
+        .get_new_address(None, None)?
+        .require_network(Network::Regtest)?;
+
+    let miner_address = miner_rpc
+        .get_new_address(None, None)?
+        .require_network(Network::Regtest)?;
 
     // Generate block to fund the miner
     miner_rpc.generate_to_address(101, &miner_address)?;
 
-    // Send 1 BTC to trader from miner
-    let amount_to_send = Amount::from_btc(1.0)?;
-    let txid = miner_rpc.send_to_address(&trader_address, amount_to_send, None, None, None, None, None, None)?;
+    // Send BTC to the trader
+    let txid = miner_rpc.send_to_address(
+        &trader_address,
+        Amount::from_btc(20.0).unwrap(),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+    )?;
 
-    // Mine 1 block to confirm transaction
+    // Transaction is in mempool, confirm it
     miner_rpc.generate_to_address(1, &miner_address)?;
 
-    // Get transaction info
-    let tx = miner_rpc.get_transaction(&txid, Some(true))?;
-    let raw_tx = miner_rpc.get_raw_transaction_info(&txid, None)?;
-
-    let block_hash = raw_tx.blockhash.ok_or("Block hash not found")?;
-    let block = miner_rpc.get_block_info(&block_hash)?;
-    let block_height = block.height;
+    // Get transaction details
+    let tx = miner_rpc.get_transaction(&txid, None)?;
+    let decoded_tx = miner_rpc.decode_raw_transaction(&tx.hex, None)?;
 
     let mut trader_output_amount = 0.0;
     let mut miner_change_amount = 0.0;
-    let mut input_address: Option<Address> = None;
 
-    for detail in &tx.details {
-        match detail.category.as_str() {
-            "receive" => trader_output_amount += detail.amount.to_btc(),
-            "send" => miner_change_amount -= detail.amount.to_btc(),
-            _ => {}
-        }
-
-        if input_address.is_none() && detail.address.is_some() {
-            input_address = detail.address.clone();
+    for vout in &decoded_tx.vout {
+        if let Some(address) = vout.script_pub_key.addresses.as_ref().and_then(|a| a.first()) {
+            if address == &trader_address {
+                trader_output_amount = vout.value.to_btc();
+            } else if address == &miner_address {
+                miner_change_amount = vout.value.to_btc();
+            }
         }
     }
 
+    // Extract input address
+    let mut input_address = None;
+    if let Some(vin) = decoded_tx.vin.first() {
+        let prev_tx = miner_rpc.get_raw_transaction(&vin.txid, None)?;
+        let decoded_prev_tx = miner_rpc.decode_raw_transaction(&prev_tx, None)?;
+
+        if let Some(prev_vout) = decoded_prev_tx.vout.get(vin.vout as usize) {
+            input_address = prev_vout
+                .script_pub_key
+                .addresses
+                .as_ref()
+                .and_then(|a| a.first())
+                .cloned();
+        }
+    }
+
+    // Calculate transaction fee
     let fee = tx
         .details
         .iter()
-        .map(|d| d.fee.unwrap_or(Amount::from_btc(0.0).unwrap()))
+        .map(|d| d.fee.unwrap_or_else(|| Amount::from_btc(0.0).unwrap()))
         .sum::<Amount>()
         .to_btc();
 
+    let block_hash = tx.info.blockhash.unwrap();
+    let block = miner_rpc.get_block_info(&block_hash)?;
+    let block_height = block.height;
+
+    // Write details to file
     let mut file = File::create("../out.txt")?;
     writeln!(file, "Transaction ID: {}", txid)?;
     writeln!(file, "Miner's Input Address: {:?}", input_address)?;
