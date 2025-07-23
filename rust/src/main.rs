a0d2197332cb206903bbaa448cd84c1a4430ca2b
thiago-Coderr <tiagoafonso110@gmail.com>
2025-07-23 21:34:11 +0100

    fixed rust cargo

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,84 +1,116 @@
+use bitcoin::{Address, Txid};
+use bitcoin::util::amount::Amount;
 use bitcoincore_rpc::{Auth, Client, RpcApi};
-use bitcoincore_rpc::bitcoin::{Amount, Txid};
+use serde_json::json;
 use std::fs::File;
 use std::io::Write;
 
-
-const RPC_URL_MINER: &str = "http://127.0.0.1:18443/wallet/Miner";
-const RPC_URL_TRADER: &str = "http://127.0.0.1:18443/wallet/Trader";
+const RPC_URL: &str = "http://127.0.0.1:18443";
 const RPC_USER: &str = "alice";
 const RPC_PASS: &str = "password";
 
+// Helper to create/load a wallet
+fn load_wallet(rpc: &Client, wallet: &str) -> bitcoincore_rpc::Result<Client> {
+    if rpc.list_wallets()?.contains(&wallet.to_string()) {
+        return Client::new(
+            &format!("{}/wallet/{}", RPC_URL, wallet),
+            Auth::UserPass(RPC_USER.to_string(), RPC_PASS.to_string()),
+        );
+    }
+
+    // If not found, create
+    rpc.create_wallet(wallet, None, None, None, None)?;
+    Client::new(
+        &format!("{}/wallet/{}", RPC_URL, wallet),
+        Auth::UserPass(RPC_USER.to_string(), RPC_PASS.to_string()),
+    )
+}
+
 fn main() -> bitcoincore_rpc::Result<()> {
-    // Connect to both wallets
-    let miner_rpc = Client::new(RPC_URL_MINER, Auth::UserPass(RPC_USER.into(), RPC_PASS.into()))?;
-    let trader_rpc = Client::new(RPC_URL_TRADER, Auth::UserPass(RPC_USER.into(), RPC_PASS.into()))?;
+    let rpc = Client::new(
+        RPC_URL,
+        Auth::UserPass(RPC_USER.to_string(), RPC_PASS.to_string()),
+    )?;
 
-    // Create wallets if they don’t exist (ignore error if already exists)
-    let root_rpc = Client::new("http://127.0.0.1:18443", Auth::UserPass(RPC_USER.into(), RPC_PASS.into()))?;
-    let _ = root_rpc.create_wallet("Miner", Some(false), None, None, None);
-    let _ = root_rpc.create_wallet("Trader", Some(false), None, None, None);
+    // Load or create wallets
+    let miner_rpc = load_wallet(&rpc, "Miner")?;
+    let trader_rpc = load_wallet(&rpc, "Trader")?;
 
-    // Generate 103 blocks to get spendable funds for Miner
-    let miner_mining_address = miner_rpc.get_new_address(None, None)?;
-    miner_rpc.generate_to_address(103, &miner_mining_address)?;
+    // Generate 101 blocks to get spendable funds in Miner
+    let miner_address = miner_rpc.get_new_address(None, None)?;
+    miner_rpc.generate_to_address(101, &miner_address)?;
 
-    // Generate Trader receiving address
+    // Trader gets a receiving address
     let trader_address = trader_rpc.get_new_address(None, None)?;
 
-    // Send 20 BTC to Trader
-    let send_amount = Amount::from_btc(20.0)?;
-    let txid = miner_rpc.send_to_address(&trader_address, send_amount, None, None, None, None, None, None)?;
+    // Send 20 BTC from Miner to Trader
+    let txid = miner_rpc.send_to_address(
+        &trader_address,
+        Amount::from_btc(20.0).unwrap(),
+        None, None, None, None, None, None,
+    )?;
 
-    // Mine 1 block to confirm
-    miner_rpc.generate_to_address(1, &miner_mining_address)?;
+    // Transaction is in mempool, confirm it
+    miner_rpc.generate_to_address(1, &miner_address)?;
 
-    // Fetch transaction details
-    let tx_result = miner_rpc.get_transaction(&txid, Some(true))?;
-    let decoded_tx = tx_result.info.decoded().unwrap();
-    let blockhash = tx_result.info.blockhash.unwrap();
-    let block = miner_rpc.get_block_header_info(&blockhash)?;
-    let blockheight = block.height;
-    let fee_btc = tx_result.info.fee.unwrap().to_btc(); // negative value
+    // Retrieve transaction info
+    let tx = miner_rpc.get_raw_transaction_info(&txid, None)?;
+    let block_hash = tx.blockhash.unwrap();
+    let block = miner_rpc.get_block_info(&block_hash)?;
+    let block_height = block.height;
 
-    // Extract input details (we assume 1 input)
-    let vin = &decoded_tx.vin[0];
-    let input_txid = vin.txid;
-    let input_vout = vin.vout;
+    // Inputs
+    let input_tx = miner_rpc.get_raw_transaction_info(&tx.vin[0].txid, None)?;
+    let miner_input_value = input_tx.vout[tx.vin[0].vout as usize].value;
 
-    let input_tx = miner_rpc.get_raw_transaction_info(&input_txid, None)?;
-    let input_vout_info = &input_tx.vout[input_vout as usize];
-    let miner_input_address = input_vout_info.script_pub_key.addresses.as_ref().unwrap()[0].clone();
-    let miner_input_amount = input_vout_info.value;
+    let input_address = input_tx.vout[tx.vin[0].vout as usize]
+        .script_pub_key
+        .addresses
+        .as_ref()
+        .unwrap()
+        .first()
+        .unwrap()
+        .clone();
 
-    // Identify outputs
+    // Outputs: find trader and change
+    let mut trader_output_address = String::new();
+    let mut trader_output_amount = 0.0;
     let mut miner_change_address = String::new();
     let mut miner_change_amount = 0.0;
-    let mut trader_output_amount = 0.0;
 
-    for vout in &decoded_tx.vout {
-        let addr = &vout.script_pub_key.addresses.as_ref().unwrap()[0];
-        if addr == &trader_address.to_string() {
+    for vout in &tx.vout {
+        let address = vout.script_pub_key.addresses
+            .as_ref()
+            .unwrap()
+            .first()
+            .unwrap()
+            .clone();
+        if address == trader_address.to_string() {
+            trader_output_address = address;
             trader_output_amount = vout.value;
         } else {
-            miner_change_address = addr.clone();
+            miner_change_address = address;
             miner_change_amount = vout.value;
         }
     }
 
-    // Write to out.txt (one line per attribute)
-    let mut file = File::create("../../out.txt")?;
-    writeln!(file, "{}", txid)?;                         // txid
-    writeln!(file, "{}", miner_input_address)?;         // miner input address
-    writeln!(file, "{}", miner_input_amount)?;          // miner input amount
-    writeln!(file, "{}", trader_address)?;              // trader output address
-    writeln!(file, "{}", trader_output_amount)?;        // trader output amount
-    writeln!(file, "{}", miner_change_address)?;        // miner change address
-    writeln!(file, "{}", miner_change_amount)?;         // miner change amount
-    writeln!(file, "{}", fee_btc)?;                     // fee (already negative)
-    writeln!(file, "{}", blockheight)?;                 // block height
-    writeln!(file, "{}", blockhash)?;                   // block hash
+    // Calculate fee
+    let fee = miner_input_value - (trader_output_amount + miner_change_amount);
+
+    // Output to file (outside of /rust)
+    let mut file = File::create("../out.txt")?;
+    writeln!(file, "{}", txid)?;
+    writeln!(file, "{}", input_address)?;
+    writeln!(file, "{:.8}", miner_input_value)?;
+    writeln!(file, "{}", trader_output_address)?;
+    writeln!(file, "{:.8}", trader_output_amount)?;
+    writeln!(file, "{}", miner_change_address)?;
+    writeln!(file, "{:.8}", miner_change_amount)?;
+    writeln!(file, "{:.8}", fee)?;
+    writeln!(file, "{}", block_height)?;
+    writeln!(file, "{}", block_hash)?;
+
+    println!("Transaction info written to ../out.txt ✅");
 
     Ok(())
 }
-
--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 # Setup nvm and install pre-req
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
-
 nvm install --lts
 npm install
 
